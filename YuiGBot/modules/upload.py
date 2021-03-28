aec68252c598b8bad5d2a528e22af52ce2f0bf2d
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-03-28 23:05:13 +0530

    Update upload.py

--- YuiGBot/modules/upload.py
+++ YuiGBot/modules/upload.py
@@ -179,18 +179,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    
-    
-    
-
-    
-__help__ = """
-    
-• /up: reply to a direct download link to upload it to telegram as files
- 
- File To Link:
- ⦁ /transfersh: reply to a telegram file to upload it on transfersh and get direct download link
- ⦁ /tmpninja: reply to a telegram file to upload it on tmpninja and get direct download link
-
-__mod_name__ = "Uoload"    
