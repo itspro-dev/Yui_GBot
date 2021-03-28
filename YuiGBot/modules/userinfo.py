9f51d852d20b176cb6278a8855d8ec8be2b3c7c7
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-03-28 14:36:32 +0530

    ok

--- YuiGBot/modules/userinfo.py
+++ YuiGBot/modules/userinfo.py
@@ -242,17 +242,17 @@ def info(update: Update, context: CallbackContext):
 
     text = (
         f"<b>┎━─━─ㄴUsᴇʀ ιɴғoㄱ</b>\n"
-        f"ID: <code>{user.id}</code>\n"
-        f"First Name: {html.escape(user.first_name)}"
+        f"✦ ID: <code>{user.id}</code>\n"
+        f"✦ First Name: {html.escape(user.first_name)}"
     )
 
     if user.last_name:
-        text += f"\nLast Name: {html.escape(user.last_name)}"
+        text += f"\n✦ Last Name: {html.escape(user.last_name)}"
 
     if user.username:
-        text += f"\nUsername: @{html.escape(user.username)}"
+        text += f"\n✦ Username: @{html.escape(user.username)}"
 
-    text += f"\nPermalink: {mention_html(user.id, 'link')}"
+    text += f"\n✦ Profile Link: {mention_html(user.id, 'link')}"
 
     if chat.type != "private" and user_id != bot.id:
         _stext = "\nPresence: <code>{}</code>"
@@ -290,7 +290,7 @@ def info(update: Update, context: CallbackContext):
         text += "\n\nThe Disaster level of this person is 'God'."
         disaster_level_present = True
     elif user.id in DEV_USERS:
-        text += "\n\nThis user is member of 'Hero Association'."
+        text += "\n\nThis user is member of 'Yui DEV'."
         disaster_level_present = True
     elif user.id in DRAGONS:
         text += "\n\nThe Disaster level of this person is 'Dragon'."
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/OnePunchUpdates/155">?</a>]'.format(
+        text += ' [<a href="https://t.me/Yui_Official">?</a>]'.format(
             bot.username
         )
 
