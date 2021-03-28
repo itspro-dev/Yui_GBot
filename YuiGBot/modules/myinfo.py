7f75ac98344ebc681f7617362e59900d0f239e8f
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-03-28 21:33:21 +0530

    Update myinfo.py

--- YuiGBot/modules/myinfo.py
+++ YuiGBot/modules/myinfo.py
@@ -14,12 +14,12 @@ async def callback_query_handler(event):
     boy = event.sender_id
     PRO = await bot.get_entity(boy)
     YuiGBot = "YOUR DETAILS \n"
-    YuiGBot += f"FIRST NAME : {YuiGBot.first_name} \n"
-    YuiGBot += f"LAST NAME : {YuiGBot.last_name}\n"
-    YuiGBot += f"YOU BOT : {YuiGBot.bot} \n"
-    YuiGBot += f"RESTRICTED : {YuiGBot.restricted} \n"
-    YuiGBot += f"USER ID : {boy}\n"
-    YuiGBot += f"USERNAME : {YuiGBot.username}\n"
+    YuiGBot += f"FIRST NAME : {first_name} \n"
+    YuiGBot += f"LAST NAME : {user.last_name}\n"
+    YuiGBot += f"YOU BOT : {bot} \n"
+    YuiGBot += f"RESTRICTED : {restricted} \n"
+    YuiGBot += f"USER ID : {user.id}\n"
+    YuiGBot += f"USERNAME : {user.username}\n"
     await event.answer(YuiGBot, alert=True)
   except Exception as e:
     await event.reply(f"{e}")
