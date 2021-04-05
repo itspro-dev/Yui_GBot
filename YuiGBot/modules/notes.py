201dea41bda7346117f9b6cee657ead91b29054f
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-04-05 14:29:41 +0530

    Update notes.py

--- YuiGBot/modules/notes.py
+++ YuiGBot/modules/notes.py
@@ -503,7 +503,7 @@ def __import_data__(chat_id, data):
 
 
 def __stats__():
-    return f"• {sql.num_notes()} notes, across {sql.num_chats()} chats."
+    return f"┝ {sql.num_notes()} Notes, Across {sql.num_chats()} Chats."
 
 
 def __migrate__(old_chat_id, new_chat_id):
